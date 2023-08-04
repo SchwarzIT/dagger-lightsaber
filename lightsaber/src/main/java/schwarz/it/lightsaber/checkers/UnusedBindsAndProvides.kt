7c9cd17db2d8d5503cd4a22f04fbf3ea721bcd61
Brais Gabín <brais.gabin@scrm.lidl>
2023-08-04 11:59:31 +0200

    Simplify UnusedBindsAndProvides

--- lightsaber/src/main/java/schwarz/it/lightsaber/checkers/UnusedBindsAndProvides.kt
+++ lightsaber/src/main/java/schwarz/it/lightsaber/checkers/UnusedBindsAndProvides.kt
@@ -13,28 +13,19 @@ internal fun checkUnusedBindsAndProvides(
     types: Types,
 ): List<Finding> {
     val usedBindsAndProvides = bindingGraph.getUsedBindsAndProvides()
-    val allUsedModulesWithItsBindings = bindingGraph.getUsedModulesWithItsBindings()
     val componentsWithItsDeclaredModules = bindingGraph.getComponentsWithItsDeclaredModules(types)
 
     return componentsWithItsDeclaredModules.flatMap { (component, declaredModulesAtThisComponent) ->
-        allUsedModulesWithItsBindings.getDeclaredAndUsedModulesWithItsBindings(declaredModulesAtThisComponent)
-            .getModulesWithUnusedBindings(usedBindsAndProvides).flatMap { (module, unusedBindings) ->
+        bindingGraph.getUsedModules()
+            .filter { module -> declaredModulesAtThisComponent.contains(module) }
+            .map { module -> module to (module.getBindings() - usedBindsAndProvides) }
+            .flatMap { (module, unusedBindings) ->
                 unusedBindings.map { binding ->
-                    Finding(
-                        component,
-                        "The $binding declared on `$module` is not used.",
-                    )
+                    Finding(component, "The $binding declared on `$module` is not used.")
                 }
             }
     }
 }
-
-private fun Map<Module, List<Module.Binding>>.getModulesWithUnusedBindings(usedBindsAndProvides: Set<Module.Binding>) =
-    map { (module, declaredBindingsAtThisModule) -> module to (declaredBindingsAtThisModule - usedBindsAndProvides) }
-
-private fun Map<Module, List<Module.Binding>>.getDeclaredAndUsedModulesWithItsBindings(
-    declaredModulesAtThisComponent: List<Module>,
-) = filter { (module, _) -> declaredModulesAtThisComponent.contains(module) }
 
 private fun BindingGraph.getComponentsWithItsDeclaredModules(
     types: Types,
@@ -42,10 +33,6 @@ private fun BindingGraph.getComponentsWithItsDeclaredModules(
     return componentNodes().associateWith { component ->
         component.getDeclaredModules(this, types).flatMap { it.toList() }
     }
-}
-
-private fun BindingGraph.getUsedModulesWithItsBindings(): Map<Module, List<Module.Binding>> {
-    return getUsedModules().associateWith { module -> module.getBindings() }
 }
 
 private fun BindingGraph.getUsedBindsAndProvides(): Set<Module.Binding> {
