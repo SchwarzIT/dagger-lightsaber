164167f4e5cd90257ac4dbcc2af088d881374a71
Brais Gabín <brais.gabin@scrm.lidl>
2023-09-14 14:22:07 +0200

    Add LightsaberTask

--- gradle-plugin/src/main/kotlin/schwarz/it/lightsaber/gradle/LightsaberPlugin.kt
+++ gradle-plugin/src/main/kotlin/schwarz/it/lightsaber/gradle/LightsaberPlugin.kt
@@ -4,6 +4,7 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.provider.Property
 import org.jetbrains.kotlin.gradle.plugin.KaptExtension
+import org.jetbrains.kotlin.gradle.tasks.BaseKapt
 
 class LightsaberPlugin : Plugin<Project> {
     override fun apply(target: Project) {
@@ -30,6 +31,35 @@ private fun Project.apply() {
             }
         }
     }
+
+    val lightsaberCheck = tasks.register("lightsaberCheck", LightsaberTask::class.java) { task ->
+        pluginManager.withPlugin("kotlin-kapt") {
+            task.dependsOn(provider { tasks.withType(BaseKapt::class.java) })
+        }
+
+        task.source = tasks.withType(BaseKapt::class.java)
+            .map { fileTree(it.destinationDir.dir("schwarz/it/lightsaber")).asFileTree }
+            .reduce { acc, fileTree -> acc.plus(fileTree) }
+            .matching { it.include("*.lightsaber") }
+
+        task.severities.set(
+            objects.mapProperty(Rule::class.java, Severity::class.java).apply {
+                Rule.entries.forEach { rule ->
+                    put(
+                        rule,
+                        when (rule) {
+                            Rule.UnusedBindInstance -> extension.unusedBindInstance
+                            Rule.UnusedBindsAndProvides -> extension.unusedBindsAndProvides
+                            Rule.UnusedDependencies -> extension.unusedDependencies
+                            Rule.UnusedModules -> extension.unusedModules
+                        },
+                    )
+                }
+            },
+        )
+    }
+
+    tasks.named("check").configure { it.dependsOn(lightsaberCheck) }
 }
 
 interface LightsaberExtension {
