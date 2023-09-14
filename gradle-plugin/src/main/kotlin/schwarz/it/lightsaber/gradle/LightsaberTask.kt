9dfda5d11bbfcf81f80a6a9896831f17e1471832
Brais Gabín <brais.gabin@scrm.lidl>
2023-09-14 14:22:07 +0200

    Add LightsaberTask

---
+++ gradle-plugin/src/main/kotlin/schwarz/it/lightsaber/gradle/LightsaberTask.kt
@@ -0,0 +1,50 @@
+package schwarz.it.lightsaber.gradle
+
+import org.gradle.api.GradleException
+import org.gradle.api.provider.MapProperty
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.SourceTask
+import org.gradle.api.tasks.TaskAction
+import org.gradle.api.tasks.VerificationTask
+import org.gradle.language.base.plugins.LifecycleBasePlugin
+
+abstract class LightsaberTask : SourceTask(), VerificationTask {
+
+    init {
+        group = LifecycleBasePlugin.VERIFICATION_GROUP
+        description = "Check for unused dagger code"
+    }
+
+    @get:Input
+    abstract val severities: MapProperty<Rule, Severity>
+
+    @TaskAction
+    fun invoke() {
+        checkIssues(source.flatMap { it.readLines() }, severities.get(), ignoreFailures)
+    }
+}
+
+internal fun checkIssues(lines: List<String>, severities: Map<Rule, Severity>, ignoreFailures: Boolean) {
+    require(lines.isNotEmpty()) { "The .lightsaber files should never be empty" }
+    val issues = lines
+        .mapNotNull { it.toIssue(severities) }
+        .onEach { println(it) }
+
+    val errorCount = issues.count { it is Error }
+    if (!ignoreFailures && errorCount > 0) {
+        val message = if (issues.count() == errorCount) {
+            if (errorCount == 1) {
+                "Analysis failed with 1 error"
+            } else {
+                "Analysis failed with $errorCount errors"
+            }
+        } else {
+            if (errorCount == 1) {
+                "Analysis failed with ${issues.count()} issues (1 was an error)"
+            } else {
+                "Analysis failed with ${issues.count()} issues ($errorCount were errors)"
+            }
+        }
+        throw GradleException(message)
+    }
+}
---
+++ gradle-plugin/src/test/kotlin/schwarz/it/lightsaber/gradle/LightsaberTaskTest.kt
@@ -0,0 +1,94 @@
+package schwarz.it.lightsaber.gradle
+
+import com.google.common.truth.Truth.assertThat
+import org.gradle.api.GradleException
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class LightsaberTaskTest {
+    private val severities = mapOf(
+        Rule.UnusedBindInstance to Severity.Error,
+        Rule.UnusedBindsAndProvides to Severity.Error,
+        Rule.UnusedDependencies to Severity.Warning,
+        Rule.UnusedModules to Severity.Ignore,
+    )
+
+    @Test
+    fun failWhenNoIssues() {
+        val e = assertThrows<IllegalArgumentException> { checkIssues(emptyList(), severities, false) }
+        assertThat(e).hasMessageThat().isEqualTo("The .lightsaber files should never be empty")
+    }
+
+    @Test
+    fun failWithOneError() {
+        val issues = listOf("position:1:1: message [UnusedBindInstance]")
+        val e = assertThrows<GradleException> { checkIssues(issues, severities, false) }
+        assertThat(e).hasMessageThat().isEqualTo("Analysis failed with 1 error")
+    }
+
+    @Test
+    fun failWithOneErrorAndOneWarning() {
+        val issues = listOf(
+            "position:1:1: message [UnusedBindInstance]",
+            "position:1:1: message [UnusedDependencies]",
+        )
+        val e = assertThrows<GradleException> { checkIssues(issues, severities, false) }
+        assertThat(e).hasMessageThat().isEqualTo("Analysis failed with 2 issues (1 was an error)")
+    }
+
+    @Test
+    fun failWithOneErrorAndTwoWarning() {
+        val issues = listOf(
+            "position:1:1: message [UnusedBindInstance]",
+            "position:1:1: message [UnusedDependencies]",
+            "position:1:2: message [UnusedDependencies]",
+        )
+        val e = assertThrows<GradleException> { checkIssues(issues, severities, false) }
+        assertThat(e).hasMessageThat().isEqualTo("Analysis failed with 3 issues (1 was an error)")
+    }
+
+    @Test
+    fun failWithTwoError() {
+        val issues = listOf(
+            "position:1:1: message [UnusedBindInstance]",
+            "position:1:2: message [UnusedBindInstance]",
+        )
+        val e = assertThrows<GradleException> { checkIssues(issues, severities, false) }
+        assertThat(e).hasMessageThat().isEqualTo("Analysis failed with 2 errors")
+    }
+
+    @Test
+    fun failWithTwoErrorAndOneWarning() {
+        val issues = listOf(
+            "position:1:1: message [UnusedBindInstance]",
+            "position:1:2: message [UnusedBindInstance]",
+            "position:1:1: message [UnusedDependencies]",
+        )
+        val e = assertThrows<GradleException> { checkIssues(issues, severities, false) }
+        assertThat(e).hasMessageThat().isEqualTo("Analysis failed with 3 issues (2 were errors)")
+    }
+
+    @Test
+    fun failWithTwoErrorAndTwoWarnings() {
+        val issues = listOf(
+            "position:1:1: message [UnusedBindInstance]",
+            "position:1:2: message [UnusedBindInstance]",
+            "position:1:1: message [UnusedDependencies]",
+            "position:1:2: message [UnusedDependencies]",
+        )
+        val e = assertThrows<GradleException> { checkIssues(issues, severities, false) }
+        assertThat(e).hasMessageThat().isEqualTo("Analysis failed with 4 issues (2 were errors)")
+    }
+
+    @Test
+    fun successWithOneWarning() {
+        val issues = listOf("position:1:1: message [UnusedDependencies]")
+        checkIssues(issues, severities, false)
+    }
+
+    @Test
+    fun successWithTwoWarning() {
+        val issues = listOf("position:1:1: message [UnusedDependencies]", "position:1:2: message [UnusedDependencies]")
+        checkIssues(issues, severities, false)
+    }
+}
