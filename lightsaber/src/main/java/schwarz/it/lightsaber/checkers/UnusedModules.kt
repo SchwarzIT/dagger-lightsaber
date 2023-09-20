ffaac6d53387e66c322470091ffe238cecced22f
Brais Gabín <brais.gabin@scrm.lidl>
2023-09-20 17:29:15 +0200

    Simplify code

--- lightsaber/src/main/java/schwarz/it/lightsaber/checkers/UnusedModules.kt
+++ lightsaber/src/main/java/schwarz/it/lightsaber/checkers/UnusedModules.kt
@@ -29,23 +29,27 @@ private fun getErrorMessages(
 ): List<String> {
     return buildList {
         if (!used.contains(node.value)) {
-            val prefix = if (path.isEmpty()) {
-                "The @Module `${node.value}`"
-            } else {
-                "The @Module `${node.value}` included by `${path.joinToString(" → ")}`"
-            }
             val usedChildren = findUsedChildren(used, node)
-            when (usedChildren.size) {
-                0 -> add("$prefix is not used.")
-                1 -> add("$prefix is not used but its child `${usedChildren.single().value}` is used.")
-                else -> add("$prefix is not used but its children ${usedChildren.joinToString { "`${it.value}`" }} are used.")
-            }
+            add(generateMessage(usedChildren.map { it.value }, node.value, path))
             val newPath = path.plus(node.value.toString())
             addAll(usedChildren.flatMap { child -> getErrorMessages(used, child, types, newPath) })
         } else {
             val newPath = path.plus(node.value.toString())
             addAll(node.children.flatMap { child -> getErrorMessages(used, child, types, newPath) })
         }
+    }
+}
+
+private fun generateMessage(usedChildren: List<Module>, node: Module, path: List<String> = emptyList()): String {
+    val prefix = if (path.isEmpty()) {
+        "The @Module `${node}`"
+    } else {
+        "The @Module `${node}` included by `${path.joinToString(" → ")}`"
+    }
+    return when (usedChildren.size) {
+        0 -> "$prefix is not used."
+        1 -> "$prefix is not used but its child `${usedChildren.single()}` is used."
+        else -> "$prefix is not used but its children ${usedChildren.joinToString { "`$it`" }} are used."
     }
 }
 
