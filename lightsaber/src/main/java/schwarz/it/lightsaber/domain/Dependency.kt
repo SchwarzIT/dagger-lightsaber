17461eb9962b7964c6ee956c63f82524e09995b0
Raúl Moreno García <raul.moreno@scrm.lidl>
2023-10-05 17:51:54 +0200

    update Dependency

--- lightsaber/src/main/java/schwarz/it/lightsaber/domain/Dependency.kt
+++ lightsaber/src/main/java/schwarz/it/lightsaber/domain/Dependency.kt
@@ -1,5 +1,6 @@
 package schwarz.it.lightsaber.domain
 
+import com.google.devtools.ksp.symbol.KSDeclaration
 import dagger.spi.model.DaggerElement
 import schwarz.it.lightsaber.utils.fold
 import javax.lang.model.element.Element
@@ -9,7 +10,11 @@ interface Dependency {
 
     companion object {
         operator fun invoke(element: DaggerElement): Dependency {
-            return element.fold(::DependencyJavac, { TODO("ksp is not supported yet") })
+            return element.fold(::DependencyJavac, { DependencyKsp(it as KSDeclaration) })
+        }
+
+        operator fun invoke(ksDeclaration: KSDeclaration): Dependency {
+            return DependencyKsp(ksDeclaration)
         }
 
         operator fun invoke(element: Element): Dependency {
@@ -22,5 +27,12 @@ interface Dependency {
 private value class DependencyJavac(private val element: Element) : Dependency {
     override fun toString(): String {
         return element.toString()
+    }
+}
+
+@JvmInline
+private value class DependencyKsp(private val ksDeclaration: KSDeclaration) : Dependency {
+    override fun toString(): String {
+        return ksDeclaration.qualifiedName!!.asString()
     }
 }
