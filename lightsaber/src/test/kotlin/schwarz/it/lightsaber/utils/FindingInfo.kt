03af3ed9667e09a03c7769f01d247aa772a8d766
Raúl Moreno García <raul.moreno@scrm.lidl>
2023-10-05 17:49:39 +0200

    update finding

--- lightsaber/src/test/kotlin/schwarz/it/lightsaber/utils/FindingInfo.kt
+++ lightsaber/src/test/kotlin/schwarz/it/lightsaber/utils/FindingInfo.kt
@@ -3,11 +3,15 @@ package schwarz.it.lightsaber.utils
 internal data class FindingInfo(
     val message: String,
     val line: Int,
-    val column: Int,
+    val column: Int?,
     val ruleName: String,
     val fileName: String = "test/MyComponent.java",
 ) {
     override fun toString(): String {
-        return "$fileName:$line:$column: $message [$ruleName]"
+        return if (column != null) {
+            "$fileName:$line:$column: $message [$ruleName]"
+        } else {
+            "$fileName:$line: $message [$ruleName]"
+        }
     }
 }
