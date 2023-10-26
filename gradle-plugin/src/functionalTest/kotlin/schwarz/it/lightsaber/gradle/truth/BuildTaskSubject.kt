e6a10d2102cf92daaf91e1155196a9a25a1868c0
Brais Gabín <brais.gabin@scrm.lidl>
2023-10-26 13:03:42 +0200

    Extend truth

---
+++ gradle-plugin/src/functionalTest/kotlin/schwarz/it/lightsaber/gradle/truth/BuildTaskSubject.kt
@@ -0,0 +1,27 @@
+package schwarz.it.lightsaber.gradle.truth
+
+import com.google.common.truth.FailureMetadata
+import com.google.common.truth.Subject
+import com.google.common.truth.Truth
+import org.gradle.testkit.runner.BuildTask
+import org.gradle.testkit.runner.TaskOutcome
+
+fun assertThat(actual: BuildTask): BuildTaskSubject {
+    return Truth.assertAbout(BuildTaskSubject.Factory).that(actual)
+}
+
+class BuildTaskSubject(
+    metadata: FailureMetadata,
+    private val actual: BuildTask,
+) : Subject(metadata, actual) {
+
+    fun hasOutcome(outcome: TaskOutcome) {
+        check("getOutcome()").that(actual.outcome).isEqualTo(outcome)
+    }
+
+    internal object Factory : Subject.Factory<BuildTaskSubject, BuildTask> {
+        override fun createSubject(failureMetadata: FailureMetadata, that: BuildTask): BuildTaskSubject {
+            return BuildTaskSubject(failureMetadata, that)
+        }
+    }
+}
