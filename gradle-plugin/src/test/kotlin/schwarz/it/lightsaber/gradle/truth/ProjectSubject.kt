7b3944cc9d6b7bb83ffd50522568272784e9f4b7
Brais Gabín <brais.gabin@scrm.lidl>
2023-11-17 11:08:21 +0100

    Gradle tests (#109)
    * Rename class
    
    * Create test for LightsaberPlugin

---
+++ gradle-plugin/src/test/kotlin/schwarz/it/lightsaber/gradle/truth/ProjectSubject.kt
@@ -0,0 +1,44 @@
+package schwarz.it.lightsaber.gradle.truth
+
+import com.google.common.truth.Fact
+import com.google.common.truth.FailureMetadata
+import com.google.common.truth.Subject
+import com.google.common.truth.Truth
+import org.gradle.api.Project
+
+fun assertThat(actual: Project?): ProjectSubject {
+    return Truth.assertAbout(ProjectSubject.Factory).that(actual)
+}
+
+class ProjectSubject(
+    metadata: FailureMetadata,
+    private val actual: Project?,
+) : Subject(metadata, actual) {
+    fun hasTask(taskName: String): TaskSubject {
+        actual!!
+
+        val task = actual.tasks.findByName(taskName)
+
+        if (task == null) {
+            failWithActual(Fact.simpleFact("The task $taskName shouldn't exist"))
+            error("WTF?") // this shouldn't be called
+        }
+
+        return assertThat(task)
+    }
+
+    fun doesntHasTask(taskName: String) {
+        actual!!
+
+        if (actual.tasks.findByName(taskName) != null) {
+            failWithActual(Fact.simpleFact("The task $taskName shouldn't exist"))
+            error("WTF?") // this shouldn't be called
+        }
+    }
+
+    internal object Factory : Subject.Factory<ProjectSubject, Project> {
+        override fun createSubject(metadata: FailureMetadata, actual: Project?): ProjectSubject {
+            return ProjectSubject(metadata, actual)
+        }
+    }
+}
