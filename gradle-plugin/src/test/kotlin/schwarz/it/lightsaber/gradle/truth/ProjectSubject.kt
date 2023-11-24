8af4b8be969f836b7659bc3afbb76dd0a434b2d8
Brais Gabín <brais.gabin@scrm.lidl>
2023-11-24 11:05:32 +0100

    Assert when lightsaber is applied

--- gradle-plugin/src/test/kotlin/schwarz/it/lightsaber/gradle/truth/ProjectSubject.kt
+++ gradle-plugin/src/test/kotlin/schwarz/it/lightsaber/gradle/truth/ProjectSubject.kt
@@ -5,6 +5,7 @@ import com.google.common.truth.FailureMetadata
 import com.google.common.truth.Subject
 import com.google.common.truth.Truth
 import org.gradle.api.Project
+import org.gradle.api.artifacts.Dependency
 
 fun assertThat(actual: Project?): ProjectSubject {
     return Truth.assertAbout(ProjectSubject.Factory).that(actual)
@@ -36,9 +37,40 @@ class ProjectSubject(
         }
     }
 
+    fun doesntHasDependency(dependency: String) {
+        actual!!
+        val coordinates = dependency.split(":")
+
+        val configuration = actual.configurations.find { configuration ->
+            configuration.dependencies.any { it.hasCoordinates(coordinates[0], coordinates[1]) }
+        }
+
+        if (configuration != null) {
+            failWithActual(Fact.simpleFact("The project has lightsaber dependency on the configuration ${configuration.name}"))
+            error("WTF?") // this shouldn't be called
+        }
+    }
+
+    fun hasConfiguration(name: String): ConfigurationSubject {
+        actual!!
+
+        val configuration = actual.configurations.findByName(name)
+
+        if (configuration == null) {
+            failWithActual(Fact.simpleFact("The configuration $name doesn't exist"))
+            error("WTF?") // this shouldn't be called
+        }
+
+        return assertThat(configuration)
+    }
+
     internal object Factory : Subject.Factory<ProjectSubject, Project> {
         override fun createSubject(metadata: FailureMetadata, actual: Project?): ProjectSubject {
             return ProjectSubject(metadata, actual)
         }
     }
+}
+
+private fun Dependency.hasCoordinates(group: String, name: String): Boolean {
+    return this.group == group && this.name == name
 }
