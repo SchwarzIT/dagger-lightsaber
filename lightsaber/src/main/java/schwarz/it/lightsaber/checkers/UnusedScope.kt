7c9be9071eed6e01ed5b56379e4685db043bbd80
Jennifer Arias <jennifer.arias@scrm.lidl>
2024-06-07 14:00:10 +0200

    UnusedScope working

--- lightsaber/src/main/java/schwarz/it/lightsaber/checkers/UnusedScope.kt
+++ lightsaber/src/main/java/schwarz/it/lightsaber/checkers/UnusedScope.kt
@@ -1,7 +1,15 @@
 package schwarz.it.lightsaber.checkers
 
+import com.google.devtools.ksp.closestClassDeclaration
+import com.google.devtools.ksp.containingFile
+import com.google.devtools.ksp.outerType
 import com.google.devtools.ksp.processing.Resolver
+import com.google.devtools.ksp.symbol.KSAnnotated
 import com.google.devtools.ksp.symbol.KSClassDeclaration
+import com.google.devtools.ksp.symbol.KSFunctionDeclaration
+import com.google.devtools.ksp.symbol.KSType
+import com.google.devtools.ksp.symbol.KSTypeReference
+import dagger.Provides
 import schwarz.it.lightsaber.Finding
 import schwarz.it.lightsaber.LightsaberKspRule
 import schwarz.it.lightsaber.toCodePosition
@@ -11,6 +19,7 @@ import javax.inject.Singleton
 internal class UnusedScopeKsp : LightsaberKspRule {
 
     private val declarations = mutableListOf<KSClassDeclaration>()
+    private val provides = mutableListOf<KSAnnotated>()
 
     override fun process(resolver: Resolver) {
         declarations.addAll(
@@ -22,14 +31,24 @@ internal class UnusedScopeKsp : LightsaberKspRule {
                 .flatMap { resolver.getSymbolsWithAnnotation(it) }
                 .filterIsInstance<KSClassDeclaration>(),
         )
+
+        provides.addAll(
+            resolver.getSymbolsWithAnnotation(Provides::class.qualifiedName!!),
+        )
     }
 
     override fun computeFindings(): List<Finding> {
-        return declarations.map {
-            Finding(
-                "The @Singleton scope is Unused.",
-                it.location.toCodePosition(),
-            )
+        val provide = provides.map {
+            (it as KSFunctionDeclaration).returnType!!.resolve()
+                .declaration.closestClassDeclaration()!!.qualifiedName!!.asString()
         }
+        return declarations
+            .filter { provide.contains(it.qualifiedName!!.asString()) }
+            .map {
+                Finding(
+                    "The @Singleton scope is Unused.",
+                    it.location.toCodePosition(),
+                )
+            }
     }
 }
