14381b22e137b2210454ed9897782db9948f8c18
Brais Gabín <brais.gabin@scrm.lidl>
2024-06-28 12:26:37 +0200

    Simplify CompilerArgumentConverter (#210)

--- lightsaber/src/test/kotlin/schwarz/it/lightsaber/utils/CompilerArgumentConverter.kt
+++ lightsaber/src/test/kotlin/schwarz/it/lightsaber/utils/CompilerArgumentConverter.kt
@@ -4,12 +4,9 @@ import org.junit.jupiter.api.extension.ParameterContext
 import org.junit.jupiter.params.converter.ArgumentConverter
 
 internal open class AbstractCompilerArgumentConverter(private vararg val rule: Rule) : ArgumentConverter {
-    final override fun convert(source: Any, context: ParameterContext): Any {
-        source as String
-        return when (source) {
-            "kapt" -> KaptKotlinCompiler(*rule)
-            "ksp" -> KspKotlinCompiler(*rule)
-            else -> error("Unknown compiler of type $source")
-        }
+    final override fun convert(source: Any, context: ParameterContext): KotlinCompiler = when (source as? String) {
+        "kapt" -> KaptKotlinCompiler(*rule)
+        "ksp" -> KspKotlinCompiler(*rule)
+        else -> throw IllegalArgumentException("Unknown compiler of type $source")
     }
 }
