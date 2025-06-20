6f1f5d8ccc468b3103ef3d56e009b5980ada6b0f
Brais Gabín <brais.gabin@scrm.lidl>
2025-06-20 11:02:20 +0200

    Make UnusedInject ksp2 compatible

--- lightsaber/src/main/java/schwarz/it/lightsaber/checkers/UnusedInject.kt
+++ lightsaber/src/main/java/schwarz/it/lightsaber/checkers/UnusedInject.kt
@@ -1,12 +1,16 @@
 package schwarz.it.lightsaber.checkers
 
+import com.google.devtools.ksp.KspExperimental
 import com.google.devtools.ksp.processing.Resolver
 import com.google.devtools.ksp.symbol.KSClassDeclaration
 import com.google.devtools.ksp.symbol.KSFunctionDeclaration
 import dagger.Provides
+import schwarz.it.lightsaber.CodePosition
 import schwarz.it.lightsaber.Finding
 import schwarz.it.lightsaber.LightsaberJavacRule
 import schwarz.it.lightsaber.LightsaberKspRule
+import schwarz.it.lightsaber.domain.Suppression
+import schwarz.it.lightsaber.domain.getSuppression
 import schwarz.it.lightsaber.domain.hasSuppress
 import schwarz.it.lightsaber.getCodePosition
 import schwarz.it.lightsaber.toCodePosition
@@ -16,38 +20,55 @@ import javax.lang.model.element.ExecutableElement
 import javax.lang.model.util.Elements
 
 internal class UnusedInjectKsp : LightsaberKspRule {
-    private val injects: MutableList<KSFunctionDeclaration> = mutableListOf()
-    private val provides: MutableSet<KSFunctionDeclaration> = mutableSetOf()
+    private val injects: MutableList<InjectInfo> = mutableListOf()
+    private val provides: MutableSet<Pair<KSFunctionDeclaration, String>> = mutableSetOf()
 
     override fun process(resolver: Resolver) {
         injects.addAll(
             resolver.getSymbolsWithAnnotation(Inject::class.qualifiedName!!)
-                .filterIsInstance<KSFunctionDeclaration>(),
+                .filterIsInstance<KSFunctionDeclaration>()
+                .map(::InjectInfo),
         )
         provides.addAll(
             resolver.getSymbolsWithAnnotation(Provides::class.qualifiedName!!)
-                .filterIsInstance<KSFunctionDeclaration>(),
+                .filterIsInstance<KSFunctionDeclaration>()
+                .map { it to it.returnType!!.resolve().declaration.qualifiedName!!.asString() },
         )
     }
 
     override fun computeFindings(): List<Finding> {
-        val providesKsTypes = provides.map { it.returnType!!.resolve() }
+        val providesKsTypes = provides.map { (_, type) -> type }.toSet()
         return injects
-            .filter { it.returnType!!.resolve() in providesKsTypes }
-            .map { inject ->
-                val provide = provides.first { it.returnType!!.resolve() == inject.returnType!!.resolve() }
+            .filter { it.type in providesKsTypes }
+            .map {
+                val provide = provides.first { (_, providesType) -> providesType == it.type }.first
                 val parent = provide.parent as KSClassDeclaration
-                val injectName = inject.parent as KSClassDeclaration
+                val injectName = it.inject.parent as KSClassDeclaration
                 Finding(
                     "The @Inject in `${injectName.qualifiedName!!.asString()}` constructor is unused because there is a @Provides defined in `${parent.qualifiedName!!.asString()}.${provide.simpleName.getShortName()}`.",
-                    inject.annotations
-                        .single { it.annotationType.resolve().declaration.qualifiedName!!.asString() == Inject::class.qualifiedName!! }
-                        .location
-                        .toCodePosition(),
-                    inject::hasSuppress,
+                    it.codePosition,
+                    it.hasSuppress,
                 )
             }
     }
+}
+
+private data class InjectInfo(
+    val inject: KSFunctionDeclaration,
+    val type: String,
+    val codePosition: CodePosition,
+    val hasSuppress: Suppression,
+) {
+    @OptIn(KspExperimental::class)
+    constructor(inject: KSFunctionDeclaration) : this(
+        inject = inject,
+        type = inject.returnType!!.resolve().declaration.qualifiedName!!.asString(),
+        codePosition = inject.annotations
+            .single { it.annotationType.resolve().declaration.qualifiedName!!.asString() == Inject::class.qualifiedName!! }
+            .location
+            .toCodePosition(),
+        hasSuppress = inject.getSuppression(),
+    )
 }
 
 internal class UnusedInjectJavac(
